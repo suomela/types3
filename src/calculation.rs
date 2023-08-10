9abae272858b6f8b60ce55a65ec80a473bd3876b
Jukka Suomela <jukka.suomela@iki.fi>
2023-08-11 00:58:09 +0300

    Refactoring

--- src/calculation.rs
+++ src/calculation.rs
@@ -1,3 +1,4 @@
+use core::marker::PhantomData;
 use crossbeam_channel::TryRecvError;
 use itertools::Itertools;
 use log::trace;
@@ -29,22 +30,135 @@ pub struct Point {
 }
 
 pub fn average_at_limit(samples: &[Sample], iter: u64, limit: u64) -> AvgResult {
-    Driver::new(samples).average_at_limit(iter, limit)
+    let (r, iter) = Driver::new(samples, AvgComp { limit }).compute(iter);
+    r.finalize(iter)
 }
 
 pub fn compare_with_points(samples: &[Sample], iter: u64, points: &[Point]) -> Vec<PointResult> {
-    Driver::new(samples).compare_with_points(iter, points)
+    let (r, iter) = Driver::new(samples, PointComp { points }).compute(iter);
+    r.finalize(iter)
 }
 
-struct Driver<'a> {
+trait Comp<R, J> {
+    fn sanity(&self);
+    fn build_total(&self) -> R;
+    fn build_tracker(&self) -> J;
+    fn step(&self, result: &mut R, tracker: &mut J, prev: u64, types: u64, size: u64) -> bool;
+}
+
+trait Tracker {}
+
+struct AvgComp {
+    limit: u64,
+}
+
+struct NoTracker {}
+
+impl Tracker for NoTracker {}
+
+struct CountTracker {
+    j: usize,
+}
+
+impl Tracker for CountTracker {}
+
+struct PointComp<'a> {
+    points: &'a [Point],
+}
+
+impl Comp<RawAvgResult, NoTracker> for AvgComp {
+    fn sanity(&self) {}
+
+    fn build_tracker(&self) -> NoTracker {
+        NoTracker {}
+    }
+
+    fn build_total(&self) -> RawAvgResult {
+        RawAvgResult::new()
+    }
+
+    fn step(
+        &self,
+        result: &mut RawAvgResult,
+        _tracker: &mut NoTracker,
+        prev: u64,
+        types: u64,
+        size: u64,
+    ) -> bool {
+        match size.cmp(&self.limit) {
+            Ordering::Less => false,
+            Ordering::Equal => {
+                result.types_low += types;
+                result.types_high += types;
+                true
+            }
+            Ordering::Greater => {
+                result.types_low += prev;
+                result.types_high += types;
+                true
+            }
+        }
+    }
+}
+
+impl Comp<RawPointResults, CountTracker> for PointComp<'_> {
+    fn sanity(&self) {
+        assert!(!self.points.is_empty());
+    }
+
+    fn build_tracker(&self) -> CountTracker {
+        CountTracker { j: 0 }
+    }
+
+    fn build_total(&self) -> RawPointResults {
+        RawPointResults {
+            results: vec![RawPointResult::new(); self.points.len()],
+        }
+    }
+
+    fn step(
+        &self,
+        result: &mut RawPointResults,
+        tracker: &mut CountTracker,
+        prev: u64,
+        types: u64,
+        size: u64,
+    ) -> bool {
+        loop {
+            let p = &self.points[tracker.j];
+            if size < p.size {
+                return false;
+            }
+            if prev < p.types {
+                result.results[tracker.j].above += 1;
+            } else if types > p.types {
+                result.results[tracker.j].below += 1;
+            }
+            tracker.j += 1;
+            if tracker.j == self.points.len() {
+                return true;
+            }
+        }
+    }
+}
+
+struct Driver<'a, T, R, J> {
     /// Input data.
     samples: &'a [Sample],
     /// All types have identifiers in `0..total_types`.
     total_types: usize,
+    comp: T,
+    _r: PhantomData<R>,
+    _j: PhantomData<J>,
 }
 
-impl Driver<'_> {
-    fn new(samples: &[Sample]) -> Driver {
+impl<T, R, J> Driver<'_, T, R, J>
+where
+    T: Send + Sync + Comp<R, J>,
+    R: Send + Sync + RawResult,
+    J: Send + Sync,
+{
+    fn new(samples: &[Sample], comp: T) -> Driver<T, R, J> {
         let mut max_type = 0;
         for sample in samples {
             for token in &sample.tokens {
@@ -55,11 +169,14 @@ impl Driver<'_> {
         Driver {
             samples,
             total_types,
+            comp,
+            _r: PhantomData,
+            _j: PhantomData,
         }
     }
 
-    fn compare_with_points(&self, iter: u64, points: &[Point]) -> Vec<PointResult> {
-        assert!(!points.is_empty());
+    fn compute(&self, iter: u64) -> (R, u64) {
+        self.comp.sanity();
         let (s1, r1) = crossbeam_channel::unbounded();
         for job in 0..RANDOM_JOBS {
             s1.send(job).expect("send succeeds");
@@ -68,7 +185,7 @@ impl Driver<'_> {
         let iter = iter_per_job * RANDOM_JOBS;
         drop(s1);
         let nthreads = num_cpus::get();
-        let mut total = vec![RawPointResult::new(); points.len()];
+        let mut total = self.comp.build_total();
         trace!("randomized, {RANDOM_JOBS} jobs, {nthreads} threads");
         thread::scope(|scope| {
             let (s2, r2) = crossbeam_channel::unbounded();
@@ -76,67 +193,11 @@ impl Driver<'_> {
                 let r1 = r1.clone();
                 let s2 = s2.clone();
                 scope.spawn(move || {
-                    let mut thread_total = vec![RawPointResult::new(); points.len()];
+                    let mut thread_total = self.comp.build_total();
                     loop {
                         match r1.try_recv() {
                             Ok(job) => {
-                                self.compare_with_points_job(
-                                    job,
-                                    iter_per_job,
-                                    points,
-                                    &mut thread_total,
-                                );
-                            }
-                            Err(TryRecvError::Empty) => unreachable!(),
-                            Err(TryRecvError::Disconnected) => break,
-                        }
-                    }
-                    s2.send(thread_total).expect("send succeeds");
-                });
-            }
-            drop(s2);
-            while let Ok(thread_total) = r2.recv() {
-                for i in 0..points.len() {
-                    total[i].add(&thread_total[i]);
-                }
-            }
-        });
-        total
-            .into_iter()
-            .map(|x| PointResult {
-                above: x.above,
-                below: x.below,
-                iter,
-            })
-            .collect_vec()
-    }
-
-    fn average_at_limit(&self, iter: u64, limit: u64) -> AvgResult {
-        let (s1, r1) = crossbeam_channel::unbounded();
-        for job in 0..RANDOM_JOBS {
-            s1.send(job).expect("send succeeds");
-        }
-        let iter_per_job = (iter + RANDOM_JOBS - 1) / RANDOM_JOBS;
-        let iter = iter_per_job * RANDOM_JOBS;
-        drop(s1);
-        let nthreads = num_cpus::get();
-        let mut total = RawAvgResult::new();
-        trace!("randomized, {RANDOM_JOBS} jobs, {nthreads} threads");
-        thread::scope(|scope| {
-            let (s2, r2) = crossbeam_channel::unbounded();
-            for _ in 0..nthreads {
-                let r1 = r1.clone();
-                let s2 = s2.clone();
-                scope.spawn(move || {
-                    let mut thread_total = RawAvgResult::new();
-                    loop {
-                        match r1.try_recv() {
-                            Ok(job) => {
-                                thread_total.add(&self.average_at_limit_job(
-                                    job,
-                                    iter_per_job,
-                                    limit,
-                                ));
+                                self.job(job, iter_per_job, &mut thread_total);
                             }
                             Err(TryRecvError::Empty) => unreachable!(),
                             Err(TryRecvError::Disconnected) => break,
@@ -150,20 +211,10 @@ impl Driver<'_> {
                 total.add(&thread_total);
             }
         });
-        AvgResult {
-            types_low: total.types_low,
-            types_high: total.types_high,
-            iter,
-        }
+        (total, iter)
     }
 
-    fn compare_with_points_job(
-        &self,
-        job: u64,
-        iter_per_job: u64,
-        points: &[Point],
-        result: &mut [RawPointResult],
-    ) {
+    fn job(&self, job: u64, iter_per_job: u64, result: &mut R) {
         let n = self.samples.len();
         let mut idx = vec![0; n];
         for (i, v) in idx.iter_mut().enumerate() {
@@ -173,81 +224,21 @@ impl Driver<'_> {
         let mut rng = Xoshiro256PlusPlus::seed_from_u64(job);
         for _ in 0..iter_per_job {
             idx.shuffle(&mut rng);
-            self.compare_with_points_calc(&idx, &mut ls, points, result);
+            self.calc_one(&idx, &mut ls, result);
         }
     }
 
-    fn average_at_limit_job(&self, job: u64, iter_per_job: u64, limit: u64) -> RawAvgResult {
-        let n = self.samples.len();
-        let mut idx = vec![0; n];
-        for (i, v) in idx.iter_mut().enumerate() {
-            *v = i;
-        }
-        let mut ls = LocalState::new(self.total_types);
-        let mut rng = Xoshiro256PlusPlus::seed_from_u64(job);
-        let mut total = RawAvgResult::new();
-        for _ in 0..iter_per_job {
-            idx.shuffle(&mut rng);
-            total.add(&self.average_at_limit_calc(&idx, &mut ls, limit));
-        }
-        total
-    }
-
-    fn average_at_limit_calc(
-        &self,
-        idx: &[usize],
-        ls: &mut LocalState,
-        limit: u64,
-    ) -> RawAvgResult {
+    fn calc_one(&self, idx: &[usize], ls: &mut LocalState, result: &mut R) {
         ls.reset();
+        let mut tracker = self.comp.build_tracker();
         for i in idx {
             let prev = ls.types;
             ls.feed_sample(&self.samples[*i]);
-            match ls.size.cmp(&limit) {
-                Ordering::Less => (),
-                Ordering::Equal => {
-                    return RawAvgResult {
-                        types_low: ls.types,
-                        types_high: ls.types,
-                    }
-                }
-                Ordering::Greater => {
-                    return RawAvgResult {
-                        types_low: prev,
-                        types_high: ls.types,
-                    }
-                }
-            }
-        }
-        unreachable!();
-    }
-
-    fn compare_with_points_calc(
-        &self,
-        idx: &[usize],
-        ls: &mut LocalState,
-        points: &[Point],
-        result: &mut [RawPointResult],
-    ) {
-        ls.reset();
-        let mut j = 0;
-        for i in idx {
-            let prev = ls.types;
-            ls.feed_sample(&self.samples[*i]);
-            loop {
-                let p = &points[j];
-                if ls.size < p.size {
-                    break;
-                }
-                if prev < p.types {
-                    result[j].above += 1;
-                } else if ls.types > p.types {
-                    result[j].below += 1;
-                }
-                j += 1;
-                if j == points.len() {
-                    return;
-                }
+            if self
+                .comp
+                .step(result, &mut tracker, prev, ls.types, ls.size)
+            {
+                return;
             }
         }
         unreachable!();
@@ -292,6 +283,10 @@ impl LocalState {
     }
 }
 
+trait RawResult {
+    fn add(&mut self, other: &Self);
+}
+
 #[derive(Clone, Copy, Deserialize, Serialize)]
 pub struct AvgResult {
     pub types_low: u64,
@@ -305,6 +300,13 @@ struct RawAvgResult {
     types_high: u64,
 }
 
+impl RawResult for RawAvgResult {
+    fn add(&mut self, other: &Self) {
+        self.types_low += other.types_low;
+        self.types_high += other.types_high;
+    }
+}
+
 impl RawAvgResult {
     fn new() -> RawAvgResult {
         RawAvgResult {
@@ -312,9 +314,13 @@ impl RawAvgResult {
             types_high: 0,
         }
     }
-    fn add(&mut self, other: &RawAvgResult) {
-        self.types_low += other.types_low;
-        self.types_high += other.types_high;
+
+    fn finalize(self, iter: u64) -> AvgResult {
+        AvgResult {
+            types_low: self.types_low,
+            types_high: self.types_high,
+            iter,
+        }
     }
 }
 
@@ -335,9 +341,35 @@ impl RawPointResult {
     fn new() -> RawPointResult {
         RawPointResult { above: 0, below: 0 }
     }
-    fn add(&mut self, other: &RawPointResult) {
+    fn add(&mut self, other: &Self) {
         self.above += other.above;
         self.below += other.below;
+    }
+}
+
+struct RawPointResults {
+    results: Vec<RawPointResult>,
+}
+
+impl RawResult for RawPointResults {
+    fn add(&mut self, other: &Self) {
+        debug_assert_eq!(self.results.len(), other.results.len());
+        for i in 0..self.results.len() {
+            self.results[i].add(&other.results[i]);
+        }
+    }
+}
+
+impl RawPointResults {
+    fn finalize(self, iter: u64) -> Vec<PointResult> {
+        self.results
+            .into_iter()
+            .map(|x| PointResult {
+                above: x.above,
+                below: x.below,
+                iter,
+            })
+            .collect_vec()
     }
 }
 
