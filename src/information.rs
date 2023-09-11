8854d742e21485586250cb5065e2d0d3faaef9a6
Jukka Suomela <jukka.suomela@iki.fi>
2023-09-12 02:10:29 +0300

    More debug printout

--- src/information.rs
+++ src/information.rs
@@ -48,14 +48,27 @@ pub fn statistics(samples: &[ISample]) {
 
 pub fn post_statistics(samples: &[CSample]) {
     let mut lemmas = HashSet::new();
+    let mut marked_lemmas = HashSet::new();
     let mut tokencount = 0;
+    let mut marked_tokencount = 0;
     for s in samples {
-        for lemma in &s.tokens {
+        for t in &s.tokens {
             tokencount += 1;
-            lemmas.insert(lemma.token);
+            lemmas.insert(t.token);
+            if t.marked {
+                marked_tokencount += 1;
+                marked_lemmas.insert(t.token);
+            }
         }
     }
     info!("after filtering: samples: {}", samples.len());
-    info!("after filtering: tokens: {}", tokencount);
-    info!("after filtering: distinct lemmas: {}", lemmas.len());
+    info!(
+        "after filtering: tokens: {}, marked: {}",
+        tokencount, marked_tokencount
+    );
+    info!(
+        "after filtering: distinct lemmas: {}, marked: {}",
+        lemmas.len(),
+        marked_lemmas.len()
+    );
 }
