a49a956217a5f51c74e202da921cf2ab7a0e493c
Jukka Suomela <jukka.suomela@iki.fi>
2024-05-19 22:10:50 +0300

    Tests for plain text export

--- integration-test/test-plot.sh
+++ integration-test/test-plot.sh
@@ -3,14 +3,16 @@
 set -e
 
 cd integration-test
-rm -rf plot
-mkdir -p plot
+rm -rf plot text
+mkdir -p plot text
 
 for a in calc/ceec-types-vs-tokens.json; do
     x="${a#calc/}"
     x="${x%.json}"
     b="plot/$x.pdf"
+    d="text/$x.txt"
     ../types3-plot --legend 'lower right' "$a" "$b"
+    ../types3-plot "$a" "$d"
 done
 
 for a in calc/*.json; do
@@ -18,8 +20,10 @@ for a in calc/*.json; do
     x="${x%.json}"
     b="plot/$x.pdf"
     c="plot/$x-wide.pdf"
+    d="text/$x.txt"
     ../types3-plot --legend 'lower right' "$a" "$b" &
     ../types3-plot --legend 'lower right' --wide "$a" "$c" &
+    ../types3-plot "$a" "$d" &
 done
 wait
 
@@ -45,4 +49,9 @@ done
 
 diff plot-expected/hash.txt plot/hash.txt
 rm -f plot-expected/hash.txt plot/hash.txt
-echo "SUCCESS: all images agree."
+
+cd text
+for a in *.txt; do
+    diff ../text-expected/$a $a
+done
+echo "SUCCESS: all images and text files agree."
